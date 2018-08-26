5799758cce0a83b032ad01ac53c2b83ef3aeac54
Nadia Lee <miss.nadia.lee@gmail.com>
2018-08-26 16:07:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,11 +12,7 @@ function distanceFromHqInFeet (someValue) {
 }
 
 function distanceTravelledInFeet (startBlock, endBlock) {
-  if (startBlock > endBlock) {
-    return (startBlock - endBlock) * 264;
-  } else {
-    return (endBlock - startBlock) * 264;
-  }
+  startBlock > endBlock ? return (startBlock - endBlock) * 264 : return (endBlock - startBlock) * 264;
 }
 
 function calculatesFarePrice (startBlock, endBlock) {
