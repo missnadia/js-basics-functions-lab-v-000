cacc062b54a2e80c0622074d9d7dbd0a33f299cc
Nadia Lee <miss.nadia.lee@gmail.com>
2018-08-26 15:42:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (someValue) {
+  if (someValue > 42) {
+    return someValue - 42;
+  } else {
+    return 42 - someValue;
+  }
+}
+
+/* function distanceFromHqInFeet (someValue) {
+  distanceFromHqInBlocks(someValue);
+  let result;
+  if () {
+
+  }
+}
+
+function distanceTravelledInFeet (startBlock, endBlock) {
+  let result;
+    if (startBlock > endBlock) {
+      result = (startBlock - endBlock) * 264;
+    } else {
+      result = (endBlock - startBlock) * 264;
+    }
+    return result
+}
+
+function calculatesFarePrice (startBlock, endBlock) {
+  if () {
+    'cannot travel that far'
+  } else if {
+
+  }
+}
